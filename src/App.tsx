7debb76689d21f3067b6f9a56ffd38a880f9b36a
tagami yutaro <yutaro.tagami@arsaga.jp>
2023-03-02 20:04:49 +0900

    add: 初期表示を関数にした。できるんだこの書き方。できると思って書いたけど

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import './App.css'
 
 function App() {
   
-  return (
-    <div className="App">
-      <h1>DELPHINIOS - Solar System Simulator</h1>
-    </div>
-  )
+  // 初回表示
+  const initialView = () => {
+    return (
+      <div className="App" onClick={() => console.log("click!!!!")}>
+        <h1>DELPHINIOS - Solar System Simulator</h1>
+        <p className='guide'>Click here.</p>
+      </div>
+    )
+  }
+
+  return initialView()
 }
 
 export default App
